b33a023dc482dd6fbca80253fd7657710f3a3ad6
unknown <marouaneshaimi.work@gmail.com>
2022-11-11 16:28:58 +0100

    navbar_on_work_4

--- src/components/Navbar/components/Avatar/index.tsx
+++ src/components/Navbar/components/Avatar/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 type avatarType = {
   ALT: string;
   IMAGE_LINK: string;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
         <div className="AVA_NOT">
           <Notification />
           <Avatar
-            ALT="..."
+            ALT="avatar"
             IMAGE_LINK="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1001.jpg"
           />
         </div>
