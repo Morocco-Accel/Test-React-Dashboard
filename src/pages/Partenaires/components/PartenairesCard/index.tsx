72e0a62a9d9d64736af66f5da88f13f4650db7a1
unknown <marouaneshaimi.work@gmail.com>
2022-11-14 11:15:12 +0100

    Responsive-On-Work-15

--- src/pages/Partenaires/components/PartenairesCard/index.tsx
+++ src/pages/Partenaires/components/PartenairesCard/index.tsx
@@ -21,25 +21,29 @@ export function Pcard({
   return (
     <div key={ID} className="CARD_P">
       <div className="CONTAINER_P">
-        <div className="ZONE_USER">
-          <img className="AVATAR" src={AVATAR_URL} alt="p_avatar" />
-          <div className="ZONE_UZ">
-            <p>{FULL_NAME}</p>
-            <div className="ZONE_CON"> {ZONES} </div>
+        <div className="CARD_LEFT">
+          <div className="ZONE_USER">
+            <img className="AVATAR" src={AVATAR_URL} alt="p_avatar" />
+            <div className="ZONE_UZ">
+              <p>{FULL_NAME}</p>
+              <div className="ZONE_CON"> {ZONES} </div>
+            </div>
           </div>
         </div>
-        <div className="ZONE_TE">
-          <p> {TELEPHONE} </p>
-          <p> {EMAIL} </p>
-        </div>
-        <div className="DISPO_P">
-          <div>
-            <span className={DISPO !== null ? "DISPO" : ""} />
-            {DISPO}
+        <div className="CARD_RIGHT">
+          <div className="ZONE_TE">
+            <p> {TELEPHONE} </p>
+            <p> {EMAIL} </p>
           </div>
-          <button className="DATE_B">
-            <i className="far fa-calendar"></i>
-          </button>
+          <div className="DISPO_P">
+            <div>
+              <span className={DISPO !== null ? "DISPO" : ""} />
+              {DISPO}
+            </div>
+            <button className="DATE_B">
+              <i className="far fa-calendar"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
